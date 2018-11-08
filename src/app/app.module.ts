8dede790ba70dbec68a5989701df427e92144680
phoenix <vanguelisk@gmail.com>
2018-11-08 18:19:09 +0200

    more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PriceBoxComponent } from './price-box/price-box.component';
+import { PassengerComponent } from './passenger/passenger.component';
+import { PassportComponent } from './passport/passport.component';
+import { PassengerListComponent } from './passenger-list/passenger-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PriceBoxComponent,
+    PassengerComponent,
+    PassportComponent,
+    PassengerListComponent
   ],
   imports: [
     BrowserModule
